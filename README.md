93244ccbfad4f47a4808d26576a97165c4712e32
Kevin Lanzon <kevinlanzon@gmail.com>
2015-09-12 13:40:04 +0100

    adds all passing test cases for the inject method

---
+++ README.md
@@ -0,0 +1,8 @@
+My Inject 2
+---------
+
+###Requirements
+
+- reopen the Array class or subclass it.
+- rewrite the inject method. Write a test for it first. Don't worry about returning an enumerator, assume a block is always given
+- name your method differently (that is, not inject() or subclass Array) because rspec uses inject() internally, so the tests will fail with weird messages unless your implementation of inject is perfect.
---
+++ spec/inject_spec.rb
@@ -0,0 +1,85 @@
+require 'inject'
+
+describe Array do
+
+  context 'without arguments' do
+    it 'can add' do
+      expect([1,2,3,4].inject { |memo, ele | memo + ele}).to eq 10
+    end
+
+    it 'can subtract' do
+      expect([1,2,3,4].inject { |memo, ele | memo - ele}).to eq (-8)
+    end
+
+    it 'can multiply' do
+      expect([1,2,3,4].inject { |memo, ele | memo * ele}).to eq 24
+    end
+
+    it 'can divide' do
+      expect([1,2,3,4].inject { |memo, ele | memo / ele}).to eq 0
+    end
+  end
+
+  context 'with arguments' do
+
+    it 'can add' do
+      expect([1,2,3,4].inject(10) { |memo, ele | memo + ele}).to eq 20
+    end
+
+    it 'can subtract' do
+      expect([1,2,3,4].inject(10) { |memo, ele | memo - ele}).to eq 0
+    end
+
+    it 'can multiply' do
+      expect([1,2,3,4].inject(10) { |memo, ele | memo * ele}).to eq 240
+    end
+
+    it 'can divide' do
+      expect([1,2,3,4].inject(10) { |memo, ele | memo / ele}).to eq 0
+    end
+  end
+
+  context 'with a symbol' do
+
+    it 'can add' do
+      expect([1,2,3,4].inject(:+)).to eq 10
+    end
+
+    it 'can subtract' do
+      expect([1,2,3,4].inject(:-)).to eq (-8)
+    end
+
+    it 'can multiply' do
+      expect([1,2,3,4].inject(:*)).to eq 24
+    end
+
+    it 'can divide' do
+      expect([1,2,3,4].inject(:/)).to eq 0
+    end
+  end
+
+  context 'with an argumant and a symbol' do
+
+    it 'can add' do
+      expect([1,2,3,4].inject(10, :+)).to eq 20
+    end
+
+    it 'can subtract' do
+      expect([1,2,3,4].inject(10, :-)).to eq 0
+    end
+
+    it 'can multiply' do
+      expect([1,2,3,4].inject(10, :*)).to eq 240
+    end
+
+    it 'can divide' do
+      expect([2,2,2,2].inject(100, :/)).to eq 6
+    end
+  end
+
+  context 'with strings' do
+    it 'can add' do
+      expect(%w(a b c d).inject { |memo, ele| memo + ' ' + ele }).to eq 'a b c d'
+    end
+  end
+end
